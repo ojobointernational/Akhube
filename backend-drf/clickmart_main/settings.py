9ee17a38473f486d03daa178444b5a44c3fe0f80
ojobointernational <91267354+ojobointernational@users.noreply.github.com>
2026-01-11 00:20:56 +0100

    static files to be loaded from nginx2

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -182,6 +182,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://172.105.34.207:5173",
-    "http://akhube.online.",
-    "https://akhube.online."
+    "http://akhube.online",
+    "https://akhube.online"
 ]
