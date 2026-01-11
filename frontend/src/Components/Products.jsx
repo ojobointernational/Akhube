76cc3ad297fc831897b2ad9dd1e09a338fb57b92
ojobointernational <91267354+ojobointernational@users.noreply.github.com>
2026-01-11 22:02:54 +0100

    update image src code

--- frontend/src/Components/Products.jsx
+++ frontend/src/Components/Products.jsx
@@ -41,7 +41,7 @@ const Products = () => {
           style={{ width: "18rem", margin: "1rem", display: "inline-block" }}
         >
           <img
-            src={product.image}
+            src={`/static/${product.image}`}
             alt={product.name}
             className="card-img-top"
             style={{ height: "200px", objectFit: "cover" }}
