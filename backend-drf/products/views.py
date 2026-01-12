5110e34ecb84bf1602fc0729f1bf62dcf52debd9
ojobointernational <91267354+ojobointernational@users.noreply.github.com>
2026-01-12 10:43:18 +0100

    update product view

--- backend-drf/products/views.py
+++ backend-drf/products/views.py
@@ -8,7 +8,17 @@ class ProductListView(generics.ListCreateAPIView):
     queryset = Product.objects.filter(is_active=True)
     serializer_class = ProductSerializer
 
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
+
 
 class ProductDetailView(generics.RetrieveAPIView):
     queryset = Product.objects.filter(is_active=True)
     serializer_class = ProductSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
