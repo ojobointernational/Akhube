256871fedd288d1c692eb924c00a63421bff09ca
ojobointernational <91267354+ojobointernational@users.noreply.github.com>
2026-01-12 12:05:09 +0100

    update product serialize22

--- backend-drf/products/serializers.py
+++ backend-drf/products/serializers.py
@@ -21,6 +21,21 @@ from .models import Product
 #         fields = ["id", "name", "price","description", "image", "stock", "tax_percent", "is_active", "created_at", "updated_at" ]
 
          
+# class ProductSerializer(serializers.ModelSerializer):
+#     image = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = Product
+#         fields = "__all__"
+
+#     def get_image(self, obj):
+#         request = self.context.get("request")
+#         if obj.image and request:
+#             #return request.build_absolute_uri(obj.image.url)
+#             return f"{settings.STATIC_URL}products/{obj.image}"
+        
+#         return None
+
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
 
@@ -29,8 +44,8 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def get_image(self, obj):
-        request = self.context.get("request")
-        if obj.image and request:
-            #return request.build_absolute_uri(obj.image.url)
+        if obj.image:  # obj.image is filename like 'apple.jpeg'
+            # Build URL relative to STATIC_URL
             return f"{settings.STATIC_URL}products/{obj.image}"
         return None
+
