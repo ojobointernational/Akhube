915c3430e8282a061aa02e5cb6ab5c05206613d6
ojobointernational <91267354+ojobointernational@users.noreply.github.com>
2026-01-12 11:07:26 +0100

    update product view2

--- backend-drf/products/views.py
+++ backend-drf/products/views.py
@@ -7,18 +7,9 @@ from .serializers import ProductSerializer
 class ProductListView(generics.ListCreateAPIView):
     queryset = Product.objects.filter(is_active=True)
     serializer_class = ProductSerializer
-
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context["request"] = self.request
-        return context
+    
 
 
 class ProductDetailView(generics.RetrieveAPIView):
     queryset = Product.objects.filter(is_active=True)
     serializer_class = ProductSerializer
-
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context["request"] = self.request
-        return context
