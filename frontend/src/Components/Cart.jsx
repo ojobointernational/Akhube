02b60186042da8e571064f74ecf679b97efc999b
ojobointernational <91267354+ojobointernational@users.noreply.github.com>
2026-01-14 13:17:42 +0100

    added updated allowed host  settings.py added secure proxy 3

--- frontend/src/Components/Cart.jsx
+++ frontend/src/Components/Cart.jsx
@@ -6,7 +6,7 @@ const Cart = () => {
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  const API_URL = "http://akhube.online/api/v1/cart/";
+  const API_URL = "https://akhube.online/api/v1/cart/";
 
   useEffect(() => {
     const fetchCart = async () => {
